1a7f53b14e37d83fdf25e17b762e5f94c7c2eca6
AbdulQadir92 <a_qadir_jamali@hotmail.com>
2022-03-11 21:29:32 +0500

    Created save blog functionality

--- src/components/Create.js
+++ src/components/Create.js
@@ -1,7 +1,43 @@
+import { useState } from 'react';
+
 const Create = () => {
+    const [title, setTitle] = useState('');
+    const [body, setBody] = useState('');
+    const [author, setAuthor] = useState('Ismail');
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const blog = {title, body, author};
+        console.log(blog);
+    } 
+
     return (
         <div className="create">
             <h2>Add a new blog</h2>
+            <form onSubmit={handleSubmit}>
+                <label>Blog title:</label>
+                <input 
+                    type="text"
+                    required="required"
+                    value={title}
+                    onChange={(e) => setTitle(e.target.value)}
+                 />
+                <label>Blog body:</label> 
+                <textarea
+                    required="required"
+                    value={body}
+                    onChange={(e) => setBody(e.target.value)}
+                >
+                </textarea>
+                <select
+                    value={author}
+                    onChange={(e) => setAuthor(e.target.value)}
+                >
+                    <option value="Ismail">Ismail</option>
+                    <option value="Samad">Samad</option>
+                </select>
+                <button>Add Blog</button>
+            </form>
         </div>
     );
 }
